0b749d27ebc9d2f4a34dbd63eda20c2a6c961e64
joaoluisro <joaoluisro@gmail.com>
2022-03-31 14:16:21 -0300

    versão final sem maquina de busca

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,8 @@ void print_board(int *board){
   }
 }
 
-// verifica se o jogo acabou
+// Verifica se o jogo acabou
+// Ele acaba quando todas as cores do tabuleiro forem iguais
 bool board_solved(int *board){
   int c1 = board[0];
   for(int i = 1 ;i < m*n; i++){
@@ -32,102 +33,142 @@ bool board_solved(int *board){
   return true;
 }
 
-// V =
-// v1: [(1,0), (1,1)]
-// v2 : [(0,0)]
-
-int dist_euclidiana(int i, int j){
-  return((n - i)*(n - i) + (m - j)*(m - j));
+// Distancia euclidiana (sem a raiz para performance) de dois pontos distintos do tabuleiro
+// dist_euclidiana = (x-i)^2 + (y - j)^2
+int dist_euclidiana(int i, int j, int corner1, int corner2){
+  return((corner1 - i)*(corner1 - i) + (corner2 - j)*(corner2 - j));
 }
 
-int calcula_distancia(int *board, int cor, int i, int j){
+// Função um pouco confusa mas extremamente elegante
+// Calcula a menor distancia euclidiana de um bloco que inicia-se na posição (i,j)
+// Faz uma comparação entre a distancia da celula e de seus vizinhos (i + 1, j) e (i, j + 1)
 
+int calcula_distancia(int *board, int cor, int i, int j, int corner1, int corner2){
+
+  // Caso base: a cor da célula não é a mesma do bloco original OU
+  //            a posição é invalida no tabuleiro 'encostou na parede'
   if(i == n || j == m || board[i*m + j] != cor){
      return MAX_INF;
   }
   else{
-    return min(dist_euclidiana(i,j),
-           min(calcula_distancia(board,cor,i+1, j),
-           calcula_distancia(board, cor, i, j + 1)));
+    // Calcule a minima distancia entre a célula atual e todas as outras
+    // células do mesmo bloco (contém a mesma cor)
+    return min(dist_euclidiana(i,j, corner1, corner2),
+           min(calcula_distancia(board,cor,i+1, j, corner1, corner2),
+           calcula_distancia(board, cor, i, j + 1, corner1, corner2)));
   }
 }
 
-int escolhe_cor_diagonal(int *board){
-  vector<pair<int,int>> fronteira;
 
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(visitado[i*m + j] && (!visitado[i*m + (j + 1)] || !visitado[(i + 1)*m + j])){
-        fronteira.push_back(make_pair(i,j));
-        //cout << i << " " <<j << endl;
-      }
-    }
+// Função que retorna a escolha gananciosa
+int bfs(int *board){
 
-  }
+  // Calcula a frequencia de cada cor no tabuleiro
+  int *frequency = new int[k + 1];
+  for(int i =0; i < k + 1; i++) frequency[i] = 0;
 
-  int cor1, cor2, dist1, dist2, min_cor;
-  int min_dist = MAX_INF;
-  int curr_i, curr_j;
-  for(int i = 0; i < fronteira.size(); i++){
-    curr_i = fronteira[i].first;
-    curr_j = fronteira[i].second;
-    cor1 = board[curr_i*m + (curr_j + 1)];
-    cor2 = board[(curr_i + 1)*m + curr_j];
-    dist1 = calcula_distancia(board, cor1, curr_i, curr_j + 1);
-    dist2 = calcula_distancia(board, cor2, curr_i + 1, curr_j);
-    if(min_dist > dist1 ){
-       min_dist = dist1;
-       min_cor = cor1;
-    }else if(min_dist > dist2){
-      min_dist = dist2;
-      min_cor = cor2;
+  for(int i = 0; i < m*n; i++){
+    if(!visitado[i]){
+      frequency[board[i]]++;
     }
   }
-  return min_cor;
-}
 
-
-
-
-// Ação do problema: Escolher uma cor
-// A heuristica é implementada aqui
-int select_color(int *board){
-  int *frequency = new int[k];
-  for(int i = 0; i < m*n; i++)frequency[board[i]]++;
-  int highest = frequency[0];
-  int index = 0;
-  for(int i = 1; i < k; i++){
-    if(frequency[k] > highest){
+  // retorna aquela que tiver o maior valor
+  int index, highest = 0;
+  for(int i = 1; i <= k; i++){
+    if(frequency[i] > highest && i != board[0]){
        index = i;
        highest = frequency[i];
     }
   }
 
-  return((rand() % k) + 1);
-  //return index + 1;
+  return index;
 }
 
-// Atualiza a matriz de cores de acordo com a decisão
-// da heuristica implementada.
+
+// Função heuristica que retorna uma cor na tentativa de 'dividir o tabuleiro'
+// 'corner1' e 'corner2' são valores de possiveis cantos do tabuleiro:
+//  inferior direito, superior direito e inferior esquerdo.
+
+// Ela procura encontrar blocos adjacentes a fronteira do flood que possuem
+// a menor distancia euclidiana do canto escolhido.
+// Selecionando a cor deste bloco como escolha para a heuristica.
+int escolhe_cor_diagonal(int *board, int corner1, int corner2){
+
+  // Inicializa a 'fronteira' do flood: valores que estão entre o 'interior' e a
+  // região não explorada do tabuleiro.
+  vector<pair<int,int>> fronteira;
+  int old_color = board[0];
+  bool is_right, is_down;
+
+  // Calcula a 'fronteira': valores que foram visitados e possuem 0's adjacentes.
+  for(int i = 0; i < n - 1; i++){
+    for(int j = 0; j < m - 1; j++){
+      is_right = !visitado[i*m + (j + 1)];
+      is_down = !visitado[(i + 1)*m + j];
+      if(visitado[i*m + j] && (is_right || is_down)) {
+        if(is_right) fronteira.push_back(make_pair(i,j + 1));
+        if(is_down) fronteira.push_back(make_pair(i + 1,j));
+      }
+    }
+  }
+
+  // Itera sobre cada elemento da fronteira, identificando 'blocos' de cores.
+  // um 'bloco' é um conjunto conectado de cores, e cada elemento da fronteira
+  // possui um ou dois blocos adjacentes. Procuramos saber qual a distancia euclidiana
+  // minima de cada bloco, isto é, a distancia minima entre todas as celulas do bloco.
+
+  int curr_cor, min_cor = board[0], dist;
+  int min_dist = MAX_INF;
+  int curr_i, curr_j;
+  for(int i = 0; i < fronteira.size(); i++){
+    // Pega o elemento adjacente da fronteira, identificado por 'curr_i' e 'curr_j'
+    curr_i = fronteira[i].first;
+    curr_j = fronteira[i].second;
+    curr_cor = board[curr_i * m + curr_j];
+
+    // Se a cor não for igual a antiga
+    if(curr_cor != old_color){
+      // Calcula a distancia desse bloco em relação ao canto escolhido
+      dist = calcula_distancia(board, curr_cor, curr_i, curr_j, corner1, corner2);
+
+      // Retorne o menor valor, e escolha a cor que é mais promissora.
+      if(min_dist > dist){
+         min_dist = dist;
+         min_cor = curr_cor;
+      }
+    }
+
+  }
+
+  return min_cor;
+}
+
+
+// Atualiza a matriz de cores de acordo com a decisão da heuristica implementada.
 // Faz a atualização de maneira recursiva baseado no algoritmo 'Flood Fill'
 void update_flood(int *board, int old_color, int choice, int i, int j){
 
-  if(i == n || j == m){
+  // Caso de borda
+  if(i == n || j == m || i < 0 || j < 0){
     return;
   }
+  // Atualizamos a matriz de visitados
   if(board[i*m + j] == old_color || board[i*m + j] == choice){
     visitado[i*m + j] = 1;
   }
-
-  if(i == n || j == m || board[i*m + j] != old_color){
-
+  // Caso no qual a cor não fara parte do novo flood
+  if(board[i*m + j] != old_color){
     return;
   }
+  // Recursão que atualiza todas as outras células adjacentes
   else{
     board[i*m + j] = choice;
     visitado[i*m + j] = 1;
     update_flood(board, old_color, choice, i + 1, j);
     update_flood(board, old_color, choice, i, j + 1);
+    update_flood(board, old_color, choice, i - 1, j);
+    update_flood(board, old_color, choice, i, j - 1);
   }
 }
 
@@ -135,25 +176,43 @@ void update_flood(int *board, int old_color, int choice, int i, int j){
 // Função que resolve o problema
 void solve(int *board){
 
+  // past_choices: escolhas feitas pela heuristica
+  // choice      : cor atual escolhida
   vector<int> past_choices;
   int choice;
 
-  do{
-    choice = select_color(board);
-  } while(choice == board[0]);
+
+  // Escolhe a primeira cor a direita da celula inicial (0,0)
+  choice = board[1];
+  // Caso no qual a cor a direita é igual, escolhe a de baixo (1,0)
+  if(choice == board[0]) choice = board[m];
+
+  // Faz um push para guardar a escolha
+  past_choices.push_back(choice);
+  // Atualiza o tabuleiro de acordo com a escolha
   update_flood(board, board[0], choice, 0, 0);
 
-  print_board(visitado);
-  cout << endl;
   // Enquanto o problema não está resolvido
-  while(!visitado[m*n]){
+  while(!board_solved(board)){
 
-    // Garante que a cor escolhida não pode ser a mesma
-    // que a anterior
+    // Se o canto inferior direito ainda não foi visitado, Tente alcança-lo
+    if(!visitado[(m*n) - 1]){
+      choice = escolhe_cor_diagonal(board, n, m);
+    }
+    // Se o canto superior direito ainda não foi visitado, Tente alcança-lo
+    else if(!visitado[(m - 1)]){
 
+      choice = escolhe_cor_diagonal(board, 0, m );
+    }
+    // Se o canto inferior esquerdo ainda não foi visitado, Tente alcança-lo
+    else if(!visitado[(n - 1)*m + 1]){
 
-    choice = escolhe_cor_diagonal(board);
-    cout << "c " << choice << endl;
+      choice = escolhe_cor_diagonal(board, n, 0);
+    }
+    // Todos os cantos foram visitados, escolha a decisão gananciosa (best-first)
+    else{
+      choice = bfs(board);
+    }
 
     // Faz um push para guardar as escolhas
     past_choices.push_back(choice);
@@ -161,16 +220,6 @@ void solve(int *board){
     // Atualiza o tabuleiro de acordo com a escolha
     update_flood(board, board[0], choice, 0, 0);
 
-    print_board(visitado);
-    cout << endl;
-    //print_board(visitado);
-    //cout << endl;
-  }
-
-  for(int i = 0; i < past_choices.size() - 1; i++){
-    if(past_choices[i] == past_choices[i+1]){
-      past_choices.erase(past_choices.begin() + i);
-    }
   }
 
   // Formata a saída
@@ -180,20 +229,9 @@ void solve(int *board){
   }
   cout << endl;
 
-
-  print_board(board);
-  cout << endl;
 }
 
 
-void search_tree(int *board){
-
-
-
-  while(!board_solved(board)){
-
-  }
-}
 
 
 int main(){
@@ -205,14 +243,12 @@ int main(){
   // acessar uma célula como board[i][j] = board[i*m + j]
   int *board = new int[n*m];
 
-  // Lê o tabuleiro
+  // Lê o tabuleiro e inicializa a matriz de visitados
   for(int i = 0; i < n*m; i++) cin >> board[i];
   for(int i = 0; i < n*m; i++) visitado[i] = 0;
+
   // Resolve o problema
   solve(board);
-
-  // Imprime a solução
-  //print_board(board);
 
   return 0;
 
